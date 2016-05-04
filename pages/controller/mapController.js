ffab46d4d435c61b414919721e8a095686329fb2
vasyltymchyshyn <vasiliy_94@hotmail.com>
2016-05-04 11:54:16 +0200

    Sistemato il controllo dell'url

--- pages/controller/mapController.js
+++ pages/controller/mapController.js
@@ -1,46 +1,52 @@
 angular.module('myApp')
-.controller('mapController', function($scope) {
+.controller('mapController', function($scope, $state) {
     var map;
     var service;
     var infowindow;
 
-    function initialize() {
+    if(sessionStorage.getItem('session')!=null) {
+        function initialize() {
 
-        var posMia = null;
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                posMia = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
+            var posMia = null;
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    posMia = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
 
-                map.setCenter(pos);
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
+                    map.setCenter(pos);
+                }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+            console.log(posMia);
+            var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: posMia,
+                zoom: 2
             });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-        console.log(posMia);
-        var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: posMia,
-            zoom: 2
-        });
 
-        var stores = JSON.parse( sessionStorage.getItem('dataItems') ).data.data;
-        console.log(stores);
-        for(var i=0; i<stores.length; i++) {
-            var pos = {lat: parseInt(stores[i].latitude), lng: parseInt(stores[i].longitude)};
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                title: stores[i].name
-            });
+            var stores = JSON.parse( sessionStorage.getItem('dataItems') ).data.data;
+            console.log(stores);
+            for(var i=0; i<stores.length; i++) {
+                var pos = {lat: parseInt(stores[i].latitude), lng: parseInt(stores[i].longitude)};
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    title: stores[i].name
+                });
+            }
         }
+        initialize();
+    } else {
+        $state.go('login');
     }
-    initialize();
+
+
 })
 
 
